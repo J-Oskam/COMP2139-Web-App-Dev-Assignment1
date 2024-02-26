70898221228de98060ef38fec7f0139ade348869
Akeen Zhong <101462287@georgebrown.ca>
2024-02-25 21:45:47 -0500

    commit before new database

--- Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
+++ Assignment1/obj/Debug/net8.0/Assignment1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e476a2469c4423111c3b805b78095d4ab5fa02a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+671bbf0d583609bfedf9e5868b477d1385e19422")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
