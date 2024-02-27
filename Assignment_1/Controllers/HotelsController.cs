b7d8d59012bb8dd3cdc00c92b1bc043df5fc5d30
Pablo <pablo.arangogomez@georgebrown.ca>
2024-02-26 21:16:46 -0500

    Bug Fixed
    Debugging done, involving main search functionality and input dates

--- Assignment_1/Controllers/HotelsController.cs
+++ Assignment_1/Controllers/HotelsController.cs
@@ -15,19 +15,22 @@ namespace Assignment_1.Controllers
             _db = db;
         }
 
-        public async Task<IActionResult> Index(string searchString, DateOnly? departureDate, DateOnly? arrivalDate, string AddFlight)
+        public async Task<IActionResult> Index(string searchString, DateOnly? departureDate, DateOnly? arrivalDate, string? AddFlight)
         {
+
             if (string.IsNullOrEmpty(AddFlight))
             {
                 HttpContext.Session.SetString("AddFlight", "No");
-                HttpContext.Session.SetString("ArrivalDate", arrivalDate.Value.ToString());
-                HttpContext.Session.SetString("DepartureDate", departureDate.Value.ToString());
             }
             else
             {
                 HttpContext.Session.SetString("AddFlight", AddFlight);
-                HttpContext.Session.SetString("ArrivalDate", arrivalDate.Value.ToString());
-                HttpContext.Session.SetString("DepartureDate", departureDate.Value.ToString());
+            }
+
+            if (arrivalDate.HasValue && departureDate.HasValue)
+            {
+                HttpContext.Session.SetString("ArrivalDate", arrivalDate.Value.ToString("yyyy-MM-dd"));
+                HttpContext.Session.SetString("DepartureDate", departureDate.Value.ToString("yyyy-MM-dd"));
             }
 
             var searchQuery = _db.Hotels
